54ac1d36b2cf83aaee15dc4ceff99d54ce6c6b34
Liz3 <accs@liz3.net>
2023-01-22 22:34:10 +0100

    smal alteration in chances

--- src/events/spawn.js
+++ src/events/spawn.js
@@ -29,7 +29,7 @@ const generateResult = (instance, guild) => {
   const colors = [...instance.config.chances_color];
   if (animal === "rabbit") {
     colors.push([
-      instance.config.guilds[guild.id].colorChance || 0.99,
+      instance.config.guilds[guild.id].colorChance || 0.97,
       [instance.config.guilds[guild.id].color],
     ]);
     colors.push(instance.config.chance_white);
