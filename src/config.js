ac569e31e2f016f9bfee880091db57705fe8efbd
Liz3 <accs@liz3.net>
2023-01-22 23:32:48 +0100

    further lower xchances

--- src/config.js
+++ src/config.js
@@ -55,9 +55,9 @@ const config = {
   },
   rabbit_colors: ["white"],
   chances_animals: [[0.9, ["rat", "ox", "tiger", "snake", "horse", "monkey", "dog", "goat"]], [0.90,["pig"]], [0.98, ["dragon"]]],
-  chance_rabbit:  [0.991, ["rabbit"]],
+  chance_rabbit:  [0.99, ["rabbit"]],
   chances_color: [[0.9, ["blue", "green", "orange", "red", "violet", "yellow"]]],
-  chance_white:  [0.999, ["white"]]
+  chance_white:  [0.995, ["white"]]
 }
 config.guildIds = Object.keys(config.guilds)
 
