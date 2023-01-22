c8c67053f348956abd394f053bc7ca6f5b0ed6de
Liz3 <accs@liz3.net>
2023-01-22 03:49:42 +0100

    add me

--- src/commands/Owner/reload.js
+++ src/commands/Owner/reload.js
@@ -11,7 +11,7 @@ export const info = {
 };
 
 export const execute = async (instance, message) => {
-  if (message.author.id !== "175408504427905025") return;
+  if (message.author.id !== "175408504427905025" && message.author.id !== "195906408561115137") return;
   const msg = message.channel.send("> // System Reloading //");
   await instance.initReload();
   await (await msg).edit("_Reload complete._");
