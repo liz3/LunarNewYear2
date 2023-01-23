0411c17bb60fadc78ad0b4592bcc58aeae7479ec
Liz3 <accs@liz3.net>
2023-01-23 06:34:43 +0100

    adjust chances

--- src/config.js
+++ src/config.js
@@ -57,7 +57,7 @@ const config = {
   chances_highroll:  [[0.7, ["rat", "ox", "tiger", "snake", "horse", "monkey", "dog", "goat"]], [0.75,["pig"]], [0.80, ["dragon"]], [0.85,["rabbit"]]],
   chances_animals: [[0.9, ["rat", "ox", "tiger", "snake", "horse", "monkey", "dog", "goat"]], [0.90,["pig"]], [0.95, ["dragon"]]],
   chance_rabbit:  [0.98, ["rabbit"]],
-  chances_color: [[0.9, ["blue", "green", "orange", "red", "violet", "yellow"]]],
+  chances_color: [[0.8, ["blue", "green", "orange", "red", "violet", "yellow"]]],
   chance_white:  [0.995, ["white"]]
 }
 config.guildIds = Object.keys(config.guilds)
