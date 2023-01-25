ffa2eb14077ffb6ab4ef596a384ecd91cf3e5370
Liz3 <accs@liz3.net>
2023-01-25 07:53:07 +0100

    add new server

--- src/config.js
+++ src/config.js
@@ -27,7 +27,8 @@ const config = {
     "708357722822672524": { name: "Yuri's Anime Zone", color: "YuriZone", invite: "https://discord.gg/yuri", whitelist: ["708357723292565516"]},
     "417702211284500482": { name: "Lost in Potato", color: "Potato", invite: "https://discord.gg/tato", whitelist: ["417863870732828672"] },
     "856951924879523871": { name: "The TeaHouse", color: "TeaHouse", invite: "https://discord.gg/sip",  whitelist: ["856959765992308747"] },
-    "683075970206531618": { name: "Paradise Isle", color: "Paradise", invite: "https://discord.gg/hot", whitelist: ["750956484111892510"] }
+    "683075970206531618": { name: "Paradise Isle", color: "Paradise", invite: "https://discord.gg/hot", whitelist: ["750956484111892510"] },
+    "519427807814615042": {name: "The X Zone", color: "Exzy", invite: "https://discord.gg/xzone", whitelist: ["888518637676269568", "1027766212320559155"], colorChance: 0.55}
   },
   animals: [
     'rat', 'ox', 'tiger', 'rabbit',
